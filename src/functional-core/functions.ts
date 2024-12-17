2a411c948fc92e1bda19fa0c318f1dbffc33a066
Dorian <pro.dorianlopez@gmail.com>
2024-12-17 16:03:16 +0100

    Ajout de nouvelles opérations mathématiques binaires et unaires, ainsi que des fonctions composées. Mise à jour des tests pour inclure la fonction incrementOf10.

--- src/functional-core/functions.ts
+++ src/functional-core/functions.ts
@@ -1,5 +1,61 @@
-type NumberToNumber = (x: number) => number;
+/**
+ * Étape 01 - Pure et totale
+ */
+export type BinaryMathOperation = (x: number, y: number) => number;
 
-const increment: NumberToNumber = (x: number): number => x + 1;
+export const add: BinaryMathOperation = (x: number, y: number) => x + y;
 
-export { increment, type NumberToNumber }
+export const multiply: BinaryMathOperation = (x: number, y: number) => x * y;
+
+export const divide: BinaryMathOperation = (x: number, y: number) => x / y;
+
+export const subtract: BinaryMathOperation = (x: number, y: number) => x - y;
+
+/**
+ * Étape 2 - Conversion de fonction binaire en fonction unitaire
+ */
+export type UnaryMathOperation = (x: number) => (y: number) => number;
+
+export const unaryAdd: UnaryMathOperation = (x: number) => (y: number) => x + y;
+
+export const unaryMultiply: UnaryMathOperation = (x: number) => (y: number) => x * y;
+
+export const unaryDivide: UnaryMathOperation = (x: number) => (y: number) => x / y;
+
+export const unarySubtract: UnaryMathOperation = (x: number) => (y: number) => x - y;
+
+/**
+ * Étape 3 - Composer une fonction incrementOf10 
+ */
+export const incrementOf10 = unaryAdd(10);
+
+/**
+ * Étape 4 - Composer une fonction ternaire
+ */
+export type TernaryMathOperationFn = (n: number, y: number, z: number) => number;
+
+export const addThenMultiply: TernaryMathOperationFn = (x: number, y: number, z: number) => multiply(add(x, y), z);
+
+/**
+ * Étape 5 - Programmez une fonction calculator prenant en paramètre une fonction f de type 
+ * UnaryMathOperation et appliquant la fonction à partir des 2 paramètres fournis à la suite.
+ */
+export const calculator = (fn: UnaryMathOperation) => (x: number) => (y: number) => fn(x);
+
+
+/**
+ * Étape 6 - 
+ */
+export type UnaryMathOperationsAggregator = (
+    ...fns: UnaryMathOperation[]
+) => (x: number) => (y: number) => number;
+
+const composeNewMathOperation: UnaryMathOperationsAggregator =
+    (...fns: UnaryMathOperation[]) =>
+        (x: number) =>
+            (y: number = x) =>
+                fns.reduce((acc, curr: UnaryMathOperation) => curr(acc)(x), x);
+
+const modulo: BinaryMathOperation = (x: number, y: number) => x % y;
+
+const idOdd = (x: number) => modulo(x, 2) ? true : false;
--- tests/increment.test.ts
+++ tests/increment.test.ts
@@ -1,7 +1,12 @@
 import { expect, test } from 'bun:test';
-import { increment } from '../src/functional-core/functions';
+//import { increment } from '../src/functional-core/functions';
+import { incrementOf10 } from '../src/functional-core/functions';
 
-test("increment should return 2 when counter = 1", () => {
-    const counter: number = 1;
-    expect(increment(counter)).toBe(2);
+// test("increment should return 2 when counter = 1", () => {
+//     const counter: number = 1;
+//     expect(increment(counter)).toBe(2);
+// });
+
+test("incrementOf10 should return +10", () => {
+    expect(incrementOf10(2)).toBe(12);
 });
